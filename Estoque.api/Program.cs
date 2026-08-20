e035867761df536e570a22b1e1f2d64c498d48ec
joaonass <joaonass04@gmail.com>
2026-08-20 13:43:17 -0300

    Finaliza projeto de cadastro de produtos

--- Estoque.api/Program.cs
+++ Estoque.api/Program.cs
@@ -29,7 +29,6 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 
-// Aplica as migrations automaticamente ao iniciar a API
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<EstoqueDbContext>();
